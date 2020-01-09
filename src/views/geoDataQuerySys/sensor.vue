fdb3812c4bd7bf9e153694201f5d8898fbeb4ab9
liyuyan <4563599@qq.com>
2020-01-10 02:21:41 +0800

    new

---
+++ src/views/geoDataQuerySys/sensor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img src="@/assets/img/sensor.png" alt="">
+    <panel-group :dialogStatus.sync="dialog" />
+  </div>
+</template>
+
+<script>
+  import PanelGroup from "./components/PanelGroup";
+  export default {
+    components: {
+      PanelGroup,
+    },
+    data() {
+      return {
+        dialog: false
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
