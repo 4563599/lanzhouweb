c5c0c0aa6f160c72ca3f49a8adbcab4ab631403f
duxin <290851906@qq.com>
2020-01-10 14:19:57 +0800

    feat: modify option

--- src/api/geoDataQuerySys.js
+++ src/api/geoDataQuerySys.js
@@ -3,56 +3,56 @@ import request from '@/utils/request'
 export default{
   get_T1(params){
     return request({
-      url: '/api/hpserver/get_T1',
+      url: '/hpserver/get_T1',
       method: 'get',
       params
     })
   },
   get_Humidity1_mean(params){
     return request({
-      url: '/api/hpserver/get_Humidity1_mean',
+      url: '/hpserver/get_Humidity1_mean',
       method: 'get',
       params
     })
   },
   get_Pressure1_mean(params){
     return request({
-      url: '/api/hpserver/get_Pressure1_mean',
+      url: '/hpserver/get_Pressure1_mean',
       method: 'get',
       params
     })
   },
   get_A1_variance(params){
     return request({
-      url: '/api/hpserver/get_A1_variance',
+      url: '/hpserver/get_A1_variance',
       method: 'get',
       params
     })
   },
   get_rainfall_mean(params){
     return request({
-      url: '/api/hpserver/get_rainfall_mean',
+      url: '/hpserver/get_rainfall_mean',
       method: 'get',
       params
     })
   },
   get_illuminance_mean(params){
     return request({
-      url: '/api/hpserver/get_illuminance_mean',
+      url: '/hpserver/get_illuminance_mean',
       method: 'get',
       params
     })
   },
   get_V1_mean(params){
     return request({
-      url: '/api/hpserver/get_V1_mean',
+      url: '/hpserver/get_V1_mean',
       method: 'get',
       params
     })
   },
   get_displacement1_mean(params){
     return request({
-      url: '/api/hpserver/get_displacement1_mean',
+      url: '/hpserver/get_displacement1_mean',
       method: 'get',
       params
     })
